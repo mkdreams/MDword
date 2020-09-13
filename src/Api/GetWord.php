4c3cb3d69e5eb25c1082e0bbf36bb00f3b8899fa
colin <709729345@qq.com>
2020-09-13 21:03:38 +0800

    add api

---
+++ src/Api/GetWord.php
@@ -0,0 +1,13 @@
+<?php
+namespace MDword\Api;
+
+
+class GetWord{
+    public function __construct() {
+        
+    }
+    
+    public function build($config) {
+        
+    }
+}
---
+++ src/Api/ParseWord.php
@@ -0,0 +1,13 @@
+<?php
+namespace MDword\Api;
+
+
+class ParseWord{
+    public function __construct() {
+        
+    }
+    
+    public function getBlockList() {
+        
+    }
+}
